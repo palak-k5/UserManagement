84f7714197ce4a5469f25682766b39659a6f449d
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2025-10-12 00:08:58 +0530

    included usertable in main file

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>User Management System</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <UserTable />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import UserTable from './components/UserTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserTable
+  }
+}
+</script>
